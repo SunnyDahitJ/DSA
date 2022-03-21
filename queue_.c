51cb9149a18ae99c8d85db3a2eec9855e32ce4f3
SunnyDahitJ <93074909+SunnyDahitJ@users.noreply.github.com>
2022-03-21 22:45:47 +0530

    Update queue_.c

--- queue_.c
+++ queue_.c
@@ -1,113 +1,112 @@
-#include<stdio.h>   
-#include<stdlib.h>  
-void insert();  
-void dequeue();  
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 50
+
+void insert();
+void delete();
 void display();
-//void sum_avg();  
-int front = -1, rear = -1 ,maxsize;  
-int queue[100];  
-int main ()  
-{  
-    int choice;   
-    printf("\n Enter the size of QUEUE : ");
-    scanf("%d",&maxsize);
-    printf("\n QUEUE OPERATIONS USING ARRAY");
-    printf("\n1.insert an element\n2.Delete an element\n3.Display the queue\n4.Exit");  
-    while(choice != 4)   
-    {     
-        printf("\nEnter your choice : ");  
-        scanf("%d",&choice);  
-        switch(choice)  
-        {  
-            case 1:  
-		            insert();  
-		            break;  
-            case 2:  
-		            dequeue();  
-		            break;  
-            case 3:  
-            		display();  
-            		break; 
-//			case 4:
-//					sum_avg();
-//					break;
-					 
-            case 4:  
-		            exit(0);  
-		            break;  
-            default:   
-            printf("\nEnter valid choice!!\n");  
-        }  
+void add();
+
+int queue_array[MAX];
+int rear = 0;
+int front = -1;
+int count=0;
+int temp=0;
+int add_item=0;
+int sum=0;
+int main()
+{
+    int choice;
+    while (1)
+    {
+        printf("1.Insert element to queue \n");
+        printf("2.Delete element from queue \n");
+        printf("3.Add and Avg \n");
+        printf("4.Exit\n");
+        printf("Enter your choice : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+        printf("Insert the element in queue : ");
+        scanf("%d",&add_item);
+            insert(add_item);
+            break;
+            case 2:
+            delete();
+            break;
+            case 3:
+            add();
+            break;
+            case 4:
+            exit(0);
+            default:
+            printf("Wrong choice \n");
+        } 
+    } 
+return 0;
+} 
+ 
+void insert(int add_item)
+{
+    if (rear == MAX - 1)
+    printf("Queue Overflow \n");
+    else
+    {
+        if (front == - 1)
+        front = 0;
+        rear = rear + 1;
+        if(add_item>=0)
+		{
+        queue_array[rear] = add_item;
+        count++;
     }
-    return 0;
-}  
-void insert()  
-{  
-    int item;  
-    printf("\nEnter the element\n");  
-    scanf("\n%d",&item);      
-    if(rear == maxsize-1)  
-    {  
-        printf("\nOVERFLOW\n");  
-        return;  
-    }  
-    if(front == -1 && rear == -1)  
-    {  
-        front = 0;  
-        rear = 0;  
-    }  
-    else   
-    {  
-        rear = rear+1;  
-    }  
-    queue[rear] = item;  
-    printf("\nValue inserted ");  
-      
-}  
-void dequeue()  
-{  
-    int item;   
-    if (front == -1 || front > rear)  
-    {  
-        printf("\nUNDERFLOW\n");  
-        return;  
-              
-    }  
-    else  
-    {  
-        item = queue[front];  
-        if(front == rear)  
-        {  
-            front = -1;  
-            rear = -1 ;  
-        }  
-        else   
-        {  
-            front = front + 1;  
-        }  
-        printf("\nvalue deleted ");  
-    }  
-      
-      
-}  
-      
-void display()  
-{  
-    int i;  
-    if(rear == -1)  
-    {  
-        printf("\nEmpty queue\n");  
-    }  
-    else  
-    {   printf("\n Elements in the queue are\n");  
-        for(i=front;i<=rear;i++)  
-        {  
-            printf("\n%d",queue[i]);  
-        }     
-    }  
+    }
 }
+ 
+void delete()
+{
+	int a;
+    if (front == - 1 )
+    {
+        printf("Queue Underflow \n");
+        return ;
+    }
+    else
+    {
+        printf("Element deleted from queue is : %d\n", queue_array[front]);
+         temp=queue_array[front];
+        front = front + 1;
+    }
+} 
+ 
+void display()
+{
+    int i;
+    if (front == - 1)
+        printf("Queue is empty \n");
+    else
+    {
+        printf("Queue is : \n");
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue_array[i]);
+        printf("\n");
+    }
+} 
 
-
-
-
-
+void add()
+{
+	int i,a;
+	float avg;
+	for(i=front;i<=cnt;i++)
+	{
+        delete();
+        sum=sum+temp;
+        insert(temp);
+        count--;
+    }
+    printf("\n%d",sum);
+    avg=(float)sum/(cnt);
+    a= cnt-front;
+    printf("\n%f",avg);
+}
